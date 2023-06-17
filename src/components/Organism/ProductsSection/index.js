a8f52f6dd175375696809e7941121b036bbbf64f
imigoroliveira <igormat4us@gmail.com>
2023-06-17 17:22:42 -0300

    Fixing problems

--- src/components/Organism/ProductsSection/index.js
+++ src/components/Organism/ProductsSection/index.js
@@ -1,48 +1,64 @@
-import { Button } from "bootstrap";
-import ButtonBase from "../../Atom/ButtonBase";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Buffer } from 'buffer';
 
-export default function ProductsSection() {
-    const products = [
-        { id: 1, name: 'Produto 1', category: 'Categoria 1', price: 10 },
-        { id: 2, name: 'Produto 2', category: 'Categoria 1', price: 20 },
-        { id: 3, name: 'Produto 3', category: 'Categoria 2', price: 15 },
-        { id: 4, name: 'Produto 4', category: 'Categoria 2', price: 25 },
-      ];
+export default function ProductsSection() { 
+  const [groupedProducts, setGroupedProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const categoriesResponse = await axios.get('http://localhost:3001/category/list');
+        const categories = categoriesResponse.data;
+
+        const productsResponse = await axios.get('http://localhost:3001/product/list');
+        const products = productsResponse.data;
+
+        // Grouping products by categories
+        const groupedProducts = categories.map(category => ({
+          category: category.name,
+          products: products.filter(product => product.category === category._id)
+        }));
+
+        setGroupedProducts(groupedProducts);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
 
-    const groupedProducts = {};
-      products.forEach((product) => {
-        if (groupedProducts[product.category]) {
-          groupedProducts[product.category].push(product);
-        } else {
-          groupedProducts[product.category] = [product];
-        }
-    });
-    
+
+
   return (
-  <div className="container">
-    {Object.keys(groupedProducts).map((category) => (
-    <div key={category}>
-      <h4 style={{ textAlign: "center", marginBottom: "30px", marginTop: "40px" }}>{category}</h4>
-      <div className="row">
-        {groupedProducts[category].map((product) => (
-          <div key={product.id} className="col-md-3">
-            <div className="card">
-              <img src={product.image} className="card-img-top" alt={product.name} />
-              <div className="card-body">
-                  <h6 className="card-title">{product.name}</h6>
-                  <p className="card-text">Preço: {product.price}</p>
-                <ButtonBase title="Ver mais..."/>
-                <br></br>
-                <ButtonBase title="Adiconar no carrinho"/>
-
+    <div className="container">
+      {groupedProducts.map(group => (
+        <div key={group.category}>
+          <h4 style={{ textAlign: "center", marginBottom: "30px", marginTop: "40px" }}>{group.category}</h4>
+          <div className="row">
+            {group.products.map(product => (
+              <div key={product._id} className="col-md-3">
+                <div className="card" style={{ width: "18rem" }}>
+                  {console.log(product.image)}
+                 {product.image ? (
+                    <img src={`data:image/jpeg;base64,${product.image}`} alt={product.name} />
+                  ) : (
+                    <img src="fallback-image.jpg" alt={product.name} />
+                  )}
+                  <div className="card-body">
+                    <h5 className="card-title">{product.name}</h5>
+                    <p className="card-text">Preço: {product.price}</p>
+                    {/* Outras informações do produto */}
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
-    ))}
-  </div>
-  )
+  );
 }
