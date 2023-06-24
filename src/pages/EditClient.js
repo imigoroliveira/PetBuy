b68f0d4db56c2c0c533c7df898fdbea0df6d37ef
imigoroliveira <igormat4us@gmail.com>
2023-06-24 01:00:18 -0300

    Fixing password trouble

--- src/pages/EditClient.js
+++ src/pages/EditClient.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import api from '../services/api';
+import bcryptjs from 'bcryptjs';
 
 export default function EditClient() {
   const [fullName, setFullName] = useState('');
@@ -9,7 +10,6 @@ export default function EditClient() {
   const [creditCardName, setCreditCardName] = useState('');
   const [creditCardNumber, setCreditCardNumber] = useState('');
   const [creditCardCvc, setCreditCardCvc] = useState('');
-
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [profileImage, setProfileImage] = useState(null);
@@ -51,6 +51,8 @@ export default function EditClient() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
+      // Criptografar a senha antes de enviar a requisição
+      const hash = await bcryptjs.hash(password, 10);
       await api.put(`client/up/${selectedClient}`, {
         fullName,
         address,
@@ -60,10 +62,10 @@ export default function EditClient() {
         creditCardNumber,
         creditCardCvc,
         email,
-        password,
+        password: hash, // Usar a senha criptografada
         profileImage,
       });
-      alert('Cliente '+ fullName +' atualizado com sucesso!');
+      alert('Cliente ' + fullName + ' atualizado com sucesso!');
     } catch (error) {
       console.error(error);
       alert('Ocorreu um erro ao atualizar o cliente.');
@@ -71,159 +73,144 @@ export default function EditClient() {
   };
 
   const handleImageChange = (event) => {
+    // Implemente a lógica necessária para lidar com a alteração da imagem de perfil, se necessário
   };
 
   return (
-    <div className="container text-center">
+    <div className="container">
       <div className="row">
-        <div className="form-custom">
+        <div className="col-md-6 offset-md-3">
           <form onSubmit={handleSubmit}>
             <div className="form-group">
-              <label>
-                Selecione um Cliente:
-                <select
-                  className="form-control"
-                  value={selectedClient}
-                  onChange={handleClientSelect}
-                >
-                  <option value="">Selecione...</option>
-                  {clientList.map((client) => (
-                    <option key={client._id} value={client.fullName}>
-                      {client.fullName}
-                    </option>
-                  ))}
-                </select>
-              </label>
+              <label htmlFor="clientSelect">Selecione um Cliente:</label>
+              <select
+                id="clientSelect"
+                className="form-control"
+                value={selectedClient}
+                onChange={handleClientSelect}
+              >
+                <option value="">Selecione...</option>
+                {clientList.map((client) => (
+                  <option key={client._id} value={client.fullName}>
+                    {client.fullName}
+                  </option>
+                ))}
+              </select>
             </div>
-            <br />
+
             <div className="form-group">
-              <label>
-                Nome Completo:
-                <input
-                  type="text"
-                  className="form-control"
-                  value={fullName}
-                  onChange={(e) => setFullName(e.target.value)}
-                />
-              </label>
+              <label htmlFor="fullName">Nome completo:</label>
+              <input
+                type="text"
+                id="fullName"
+                className="form-control"
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
+              />
             </div>
-            <br />
+
             <div className="form-group">
-              <label>
-                Endereço:
-                <input
-                  type="text"
-                  className="form-control"
-                  value={address}
-                 
-                  onChange={(e) => setAddress(e.target.value)}
-                  />
-                </label>
-              </div>
-              <br />
-              <div className="form-group">
-                <label>
-                  Telefone:
-                  <input
-                    type="text"
-                    className="form-control"
-                    value={tel}
-                    onChange={(e) => setTel(e.target.value)}
-                  />
-                </label>
-              </div>
-              <br />
-              <div className="form-group">
-                <label>
-                  CPF:
-                  <input
-                    type="text"
-                    className="form-control"
-                    value={cpf}
-                    onChange={(e) => setCpf(e.target.value)}
-                  />
-                </label>
-              </div>
-              <br />
-              
-              <div className="form-group">
-                <label>
-                    Credit Card's Name:
-                  <input
-                    type="text"
-                    className="form-control"
-                    value={creditCardName}
-                    onChange={(e) => setCreditCardName(e.target.value)}
-                  />
-                </label>
-              </div>
-              <br />
-              <div className="form-group">
-                <label>
-                    Credit Card's Number:
-                  <input
-                    type="text"
-                    className="form-control"
-                    value={creditCardNumber}
-                    onChange={(e) => setCreditCardNumber(e.target.value)}
-                  />
-                </label>
-              </div>
-              <br />
-              <div className="form-group">
-                <label>
-                  Credit Card's Cvc
-                  <input
-                    type="text"
-                    className="form-control"
-                    value={creditCardCvc}
-                    onChange={(e) => setCreditCardCvc(e.target.value)}
-                  />
-                </label>
-              </div>
-              <br />
-              <div className="form-group">
-                <label>
-                  E-mail:
-                  <input
-                    type="email"
-                    className="form-control"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </label>
-              </div>
-              <br />
-              <div className="form-group">
-                <label>
-                  Senha:
-                  <input
-                    type="password"
-                    className="form-control"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </label>
-              </div>
-              <br />
-              <div className="form-group">
-                <label>
-                  Imagem de Perfil:
-                  <input
-                    type="file"
-                    className="form-control-file"
-                    onChange={handleImageChange}
-                  />
-                </label>
-              </div>
-              <br />
-              <button type="submit" className="btn btn-primary">
-                Salvar
-              </button>
-            </form>
-          </div>
+              <label htmlFor="address">Endereço:</label>
+              <input
+                type="text"
+                id="address"
+                className="form-control"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="tel">Telefone:</label>
+              <input
+                type="text"
+                id="tel"
+                className="form-control"
+                value={tel}
+                onChange={(e) => setTel(e.target.value)}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="cpf">CPF:</label>
+              <input
+                type="text"
+                id="cpf"
+                className="form-control"
+                value={cpf}
+                onChange={(e) => setCpf(e.target.value)}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="creditCardName">Nome do cartão de crédito:</label>
+              <input
+                type="text"
+                id="creditCardName"
+                className="form-control"
+                value={creditCardName}
+                onChange={(e) => setCreditCardName(e.target.value)}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="creditCardNumber">Número do cartão de crédito:</label>
+              <input
+                type="text"
+                id="creditCardNumber"
+                className="form-control"
+                value={creditCardNumber}
+                onChange={(e) => setCreditCardNumber(e.target.value)}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="creditCardCvc">CVC do cartão de crédito:</label>
+              <input
+                type="text"
+                id="creditCardCvc"
+                className="form-control"
+                value={creditCardCvc}
+                onChange={(e) => setCreditCardCvc(e.target.value)}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                className="form-control"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="password">Senha:</label>
+              <input
+                type="password"
+                id="password"
+                className="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="profileImage">Imagem de perfil:</label>
+              <input
+                type="file"
+                id="profileImage"
+                className="form-control-file"
+                onChange={handleImageChange}
+              />
+            </div>
+
+            <button type="submit" className="btn btn-primary">Atualizar Cliente</button>
+          </form>
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
